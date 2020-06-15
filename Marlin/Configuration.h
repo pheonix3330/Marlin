d49239b6f289efd23d2d2ccfd2f282401a37d85f
Eddie <pheonix3330@gmail.com>
2020-06-15 19:42:53 -0400

    flip y direction and update offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1073,7 +1073,7 @@
 
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
 #define INVERT_X_DIR true
-#define INVERT_Y_DIR true
+#define INVERT_Y_DIR false
 #define INVERT_Z_DIR false
 
 // @section extruder
@@ -1113,7 +1113,7 @@
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
-#define Y_MIN_POS -15
+#define Y_MIN_POS -10
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
