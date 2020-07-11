af2b8c8eb497d8de74a623c0246123267165cfbd
Eddie <pheonix3330@gmail.com>
2020-07-11 19:51:38 -0400

    Update thermistor to slice engineering and updated ezabl probe offsets

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -416,7 +416,7 @@
  *   998 : Dummy Table that ALWAYS reads 25°C or the temperature defined below.
  *   999 : Dummy Table that ALWAYS reads 100°C or the temperature defined below.
  */
-#define TEMP_SENSOR_0 1
+#define TEMP_SENSOR_0 67
 #define TEMP_SENSOR_1 0
 #define TEMP_SENSOR_2 0
 #define TEMP_SENSOR_3 0
@@ -980,7 +980,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { 48, -5, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { 53.5, 10.35, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
