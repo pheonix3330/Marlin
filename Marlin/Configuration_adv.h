26da2782ad3e12e53ef6c367134e34afb0ef6a76
Eddie <62308091+pheonix3330@users.noreply.github.com>
2020-07-27 10:39:15 -0400

    Update Configuration_adv.h
    Change Tramming Points

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -775,7 +775,7 @@
 #if ENABLED(ASSISTED_TRAMMING)
 
   // Define positions for probing points, use the hotend as reference not the sensor.
-  #define TRAMMING_POINT_XY { {  20, 20 }, { 200,  20 }, { 200, 200 }, { 20, 200 } }
+  #define TRAMMING_POINT_XY { { 32, 32 }, { 203, 32 }, { 203, 203 }, { 32, 203 } }
 
   // Define positions names for probing points.
   #define TRAMMING_POINT_NAME_1 "Front-Left"
