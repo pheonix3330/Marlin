9547370af8c9bec97cc9549fc18f244c17716c89
Eddie <pheonix3330@gmail.com>
2020-09-27 19:12:36 -0400

    change machine name

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -132,7 +132,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Ender 3 SKR Mini E3"
+#define CUSTOM_MACHINE_NAME "SKR E3 V2"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
